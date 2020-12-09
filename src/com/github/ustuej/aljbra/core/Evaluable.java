8809d0f0187bc668ecc8194f839ded5110e415c3
UStuej <bushytail4@gmail.com>
2020-12-09 12:04:10 -0600

    Initial commit

---
+++ src/com/github/ustuej/aljbra/core/Evaluable.java
@@ -0,0 +1,7 @@
+package com.github.ustuej.aljbra.core;
+
+public interface Evaluable {
+	
+	void eval();  // FIXME Replace void with an actual numerical type to return.
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module com.github.ustuej.aljbra {
+	exports com.github.ustuej.aljbra.core;
+}
